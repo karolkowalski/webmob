da21474e9615ed9da5789bdad9de90a7a44f0649
Cédric Marfil <cedric.marfil@fr.enersys.com>
2013-03-13 12:24:45 +0100

    TP2: Nouvelle structure pour éviter la recopie

---
+++ www/footer.php
@@ -0,0 +1,15 @@
+			</div>
+		</div>
+		<br class="clear"/>
+		
+		<nav id="footer">
+			<ul>
+				<li><a href="/search.php">Search</a></li>
+				<li><a href="/last.php">Last</a></li>
+				<li><a href="/random.php">Random</a></li>
+				<li><a href="/legal.php">Legal</a></li>
+			</ul>
+		</nav>
+	</body>
+	
+</html>
--- www/search.php
+++ www/search.php
@@ -1,43 +1,4 @@
-<!doctype html>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="HandheldFriendly" content="true"/>
-		<meta name="MobileOptimized" content="320"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		<link rel="stylesheet" href="./jss/style.css">
-		<link rel="shortcut icon" href="./img/favicon.ico">
-		<link rel="apple-touch-icon" href="./img/logo-57.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="./img/logo-72.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="./img/logo-114.png">
-		<link rel="apple-touch-startup-image" href="./img/splash-touch.png">
-	</head>
-	<body>
-		<div class="wrapper">
-			<a id="header" href="/index.php">
-				<img class="logo" src="./img/logo-72.png"/>
-				<div class="titre">
-					<h1>MyBD.fr</h1>
-					<h2>pleins d'infos sur les BDs</h2>
-				</div>
-			</a>
-		</div>
-		<br class="clear"/>
-
-		<div class="wrapper">
-			<div id="menu">
-				<ul>
-					<li><a href="/search.php">Search</a></li>
-					<li><a href="/last.php">Last</a></li>
-					<li><a href="/random.php">Random</a></li>
-					<li><a href="/legal.php">Legal</a></li>
-				</ul>
-			</div>
-		</div>
-		<br class="clear"/>
-
-		<div class="wrapper">
-			<div class="content">
+<?php include 'header.php'; ?>
 				<form method="GET" action="search.php">
 					<label for="auteur">Auteur: </label><input type="text" id="auteur" name="auteur"/>
 					<label for="nationalite">Nationalité: </label><input type="text" id="nationalite" name="nationalite"/>
@@ -47,18 +8,4 @@
 					<label for="prix">Prix: </label><input type="text" id="prix" name="prix"/>
 					<label for="disponibilite">Disponibilité: </label><input type="checkbox" id="disponibilite" name="disponibilite"/>
 				</form>
-			</div>
-		</div>
-		<br class="clear"/>
-
-		<div id="footer">
-			<ul>
-				<li><a href="/search.php">Search</a></li>
-				<li><a href="/last.php">Last</a></li>
-				<li><a href="/random.php">Random</a></li>
-				<li><a href="/legal.php">Legal</a></li>
-			</ul>
-		</div>
-	</body>
-	
-</html>
+<?php include 'footer.php'; ?>
