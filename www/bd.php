1ec13edbff84dd8fb69d8bac573f5b99e2d6dc7d
Cédric Marfil <cedric.marfil@fr.enersys.com>
2013-03-13 12:14:36 +0100

    Fin du TP 1

---
+++ www/bd.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="HandheldFriendly" content="true"/>
+		<meta name="MobileOptimized" content="320"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel="stylesheet" href="./jss/style.css">
+		<link rel="shortcut icon" href="./img/favicon.ico">
+		<link rel="apple-touch-icon" href="./img/logo-57.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="./img/logo-72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="./img/logo-114.png">
+		<link rel="apple-touch-startup-image" href="./img/splash-touch.png">
+	</head>
+	<body>
+		<div class="wrapper">
+			<a id="header" href="/index.php">
+				<img class="logo" src="./img/logo-72.png"/>
+				<div class="titre">
+					<h1>MyBD.fr</h1>
+					<h2>pleins d'infos sur les BDs</h2>
+				</div>
+			</a>
+		</div>
+		<br class="clear"/>
+
+		<div class="wrapper">
+			<div id="menu">
+				<ul>
+					<li><a href="/search.php">Search</a></li>
+					<li><a href="/last.php">Last</a></li>
+					<li><a href="/random.php">Random</a></li>
+					<li><a href="/legal.php">Legal</a></li>
+				</ul>
+			</div>
+		</div>
+		<br class="clear"/>
+
+		<div class="wrapper">
+			<div class="content">
+				Détail Bd
+			</div>
+		</div>
+		<br class="clear"/>
+		
+		<div id="footer">
+			<ul>
+				<li><a href="/search.php">Search</a></li>
+				<li><a href="/last.php">Last</a></li>
+				<li><a href="/random.php">Random</a></li>
+				<li><a href="/legal.php">Legal</a></li>
+			</ul>
+		</div>
+	</body>
+	
+</html>
---
+++ www/legal.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="HandheldFriendly" content="true"/>
+		<meta name="MobileOptimized" content="320"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel="stylesheet" href="./jss/style.css">
+		<link rel="shortcut icon" href="./img/favicon.ico">
+		<link rel="apple-touch-icon" href="./img/logo-57.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="./img/logo-72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="./img/logo-114.png">
+		<link rel="apple-touch-startup-image" href="./img/splash-touch.png">
+	</head>
+	<body>
+		<div class="wrapper">
+			<a id="header" href="/index.php">
+				<img class="logo" src="./img/logo-72.png"/>
+				<div class="titre">
+					<h1>MyBD.fr</h1>
+					<h2>pleins d'infos sur les BDs</h2>
+				</div>
+			</a>
+		</div>
+		<br class="clear"/>
+
+		<div class="wrapper">
+			<div id="menu">
+				<ul>
+					<li><a href="/search.php">Search</a></li>
+					<li><a href="/last.php">Last</a></li>
+					<li><a href="/random.php">Random</a></li>
+					<li><a href="/legal.php">Legal</a></li>
+				</ul>
+			</div>
+		</div>
+		<br class="clear"/>
+
+		<div class="wrapper">
+			<div class="content">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut leo mauris, sit amet laoreet risus. Praesent laoreet aliquet nunc, a tincidunt sem pellentesque sed. Maecenas nisi mi, congue nec suscipit elementum, imperdiet ac libero. Fusce sed dolor eu enim fringilla convallis. Donec dapibus augue in mauris vestibulum in interdum lorem pretium. Nunc eget dui ut leo pulvinar aliquet. Aenean non dolor quis tellus semper tempus non sed ipsum. Fusce commodo vestibulum justo, non bibendum erat feugiat nec. Suspendisse viverra gravida ullamcorper. Donec iaculis tincidunt interdum. Morbi sodales erat at nunc luctus dapibus. Phasellus nisl leo, ornare vel consectetur vel, congue ut mi. Mauris eleifend enim tellus, sed aliquet nulla.</p>
+				<p>Nunc ante purus, dignissim sed fringilla sit amet, rhoncus at erat. Suspendisse ultricies nisi sed ante ornare sit amet varius dui egestas. Suspendisse dui leo, ullamcorper ac euismod eu, ullamcorper eget ligula. Phasellus ac elit at odio cursus volutpat ac non diam. Integer urna nisl, placerat nec eleifend nec, fermentum tincidunt est. Morbi vel tincidunt magna. Sed non lectus felis, ac eleifend quam. Suspendisse accumsan posuere risus viverra feugiat. Praesent at nisi et lectus hendrerit fermentum ac at enim.</p>
+				<p>Mauris ultrices ante in orci mollis vehicula. Sed mollis massa ac dui luctus tincidunt eu commodo libero. In id diam id massa luctus luctus quis vitae mauris. Fusce accumsan consequat molestie. Phasellus auctor urna a nulla commodo ut ultrices felis pharetra. Donec euismod nunc nec tellus ultrices adipiscing. Aenean sed orci sit amet tellus feugiat faucibus. Praesent ipsum felis, fringilla vitae semper et, egestas blandit risus. Proin metus nisl, pharetra a dictum ut, dapibus ac elit.</p>
+				<p>Donec ultricies nulla vitae lacus commodo sed rutrum risus ullamcorper. Duis scelerisque sollicitudin mauris, sed dictum risus bibendum eu. Praesent cursus faucibus eros, ac feugiat tortor rutrum eu. Nullam mollis euismod porttitor. Morbi eget mauris a erat gravida porta. Proin at nisl ligula, id viverra tortor. Fusce congue facilisis diam sit amet sagittis. Aliquam fringilla augue at nisl sollicitudin sollicitudin nec ac nisi.</p>
+				<p>Morbi cursus elit sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus placerat eros quis metus pellentesque vel commodo est blandit. Donec elementum tortor eget leo placerat at consectetur nulla luctus. Pellentesque euismod pellentesque ante, rhoncus fringilla orci sodales id. Sed euismod enim dui, vel auctor urna. Donec congue metus ut leo ultrices id scelerisque elit blandit. Maecenas imperdiet quam sed sem porta vitae tempus leo pellentesque. Vivamus rhoncus, dolor at dictum aliquam, justo justo ornare sapien, ac sollicitudin nibh arcu ut leo. Sed dolor dolor, posuere in malesuada vitae, posuere quis tellus. Mauris augue ligula, dapibus vitae suscipit ut, mollis ut lacus. Aliquam erat volutpat. Pellentesque vel nunc magna.</p>
+			</div>
+		</div>
+		<br class="clear"/>
+
+		<div id="footer">
+			<ul>
+				<li><a href="/search.php">Search</a></li>
+				<li><a href="/last.php">Last</a></li>
+				<li><a href="/random.php">Random</a></li>
+				<li><a href="/legal.php">Legal</a></li>
+			</ul>
+		</div>
+	</body>
+			
+</html>
