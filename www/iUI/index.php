d8e118608a4fae1428662b26c12e620f23324851
Cédric Marfil <cedric.marfil@fr.enersys.com>
2013-03-14 09:56:10 +0100

    TP3.4: Fin de Tâche

--- www/iUI/index.php
+++ www/iUI/index.php
@@ -15,7 +15,28 @@
 		</div>
 				
 		<div id="random" title="Random" class="panel">
-			Random information
+			Random information<br/>
+			<div id="location"></div>
+			<button onclick="updateLocation()">Update Location</button>
+			<script>
+				geo.init();
+				function updateLocation() {
+					var innerHtml = "";
+					var geoLocation = geo.get();
+					if (geoLocation != null) {
+						innerHtml += "Voici votre position:<br/>";
+						innerHtml += " - Longitude: " + geoLocation.coords.longitude + "<br/>";
+						innerHtml += " - Latitude: " + geoLocation.coords.latitude + "<br/>";
+						innerHtml += " - Altitude: " + geoLocation.coords.altitude + "<br/>";
+					} else {
+						var geoError = library.get(geo.LS_ERROR_KEY);
+						if (geoError != null) {
+							innerHtml += geoError + "<br/>";
+						}
+					}
+					document.getElementById('location').innerHTML=innerHtml;
+				}
+			</script>
 		</div>
 				
 		<div id="legal" title="Legal" class="panel">
